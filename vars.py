d5e014a976211dff887c6d4e02cd6380dc1de618
Kamlesh Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2025-06-05 11:37:39 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -6,3 +6,4 @@ from os import environ
 API_ID = "1144902" #Replace with your api id
 API_HASH = "e743e5a4f35076e4c558a4bd713082e9" #Replace with your api hash
 BOT_TOKEN = "7907920004:AAG18EpyWZKX27apayhahGDsQ8tB3NqBW-0" #Replace with your bot token
+OWNER_ID = "7788212890"
