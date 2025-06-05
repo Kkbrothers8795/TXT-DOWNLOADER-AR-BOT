c0cd02a35a8b910573dedfa4bb2280588133aec0
Kamlesh Saini <150409757+Kkbrothers8795@users.noreply.github.com>
2025-06-05 18:33:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -608,6 +608,13 @@ async def upload(bot: Client, m: Message):
                 name = f'{str(count).zfill(3)}) {name1[:60]}'
                 cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
 
+            elif "https://appx-transcoded-videos-mcdn.akamai.net.in/videos/yesofficer-data/" in url:
+                url = url.replace("https://appx-transcoded-videos-mcdn.akamai.net.in/videos/yesofficer-data/", "")
+                name1 = links[i][0].replace("\t", "").replace(":", "").replace("/", "").replace("+", "").replace("#", "").replace("|", "").replace("@", "@").replace("*", "").replace(".", "").replace("https", "").replace("http", "").strip()
+                name = f'{str(count).zfill(3)}) {name1[:60]}'
+                cmd = f'yt-dlp -o "{name}%(ext)s" "{url}"'
+
+
             elif "apps-s3-jw-prod.utkarshapp.com" in url:
                 if 'enc_plain_mp4' in url:
                     url = url.replace(url.split("/")[-1], res+'.mp4')
